b066e0dcf8354f7a8f0081b2f6bfb71ddd800691
chriscesar <chris.cesar@environment-agency.gov.uk>
2024-04-25 11:30:21 +0100

    Started work on colour-coded version (WIP)

--- R/an.EA_wordcloud.R
+++ R/an.EA_wordcloud.R
@@ -63,8 +63,13 @@ tx_chk <- tx_chk0 %>%
   #dplyr::select(.,-ScientificName) %>% 
   distinct()
 
-tx_chk %>% dplyr::select(., Taxa, Aphia.ID) %>% 
+tx_chk %>%
+  dplyr::select(., Taxa, Aphia.ID) %>% 
   distinct() -> tx_chktrm
+
+tx_chk %>%
+  dplyr::select(., -c(ReturnedTaxName)) %>%
+  distinct() -> tx_chktrm2
 
 df_tx0 <- left_join(df_tx0, tx_chktrm, by="Aphia.ID")
 df_tx0$Taxa.x <- df_tx0$Taxa.y;df_tx0$Taxa.y <- NULL
@@ -76,10 +81,11 @@ df_tx0 %>%
   ungroup() %>% 
   as_tibble(.) -> df_tx
 rm(tx_chk,tx_chk0,tx_chktrm)
+toc()
 
 ###############
 ## summarise by counts & plot cloud
-## LF
+# LF version ####
 tic();set.seed(271);df_LF0 %>% 
   # group_by(Region) %>% 
   count(.,LF02) %>% 
@@ -97,12 +103,13 @@ tic();set.seed(271);df_LF0 %>%
                                   size=14,
                                   colour="red")) -> pl_LF_wc
 
-png(file = "figs/wordcloud_LF_v3.png",
+png(file = "figs/wordcloud_LF_v3.5.png",
     width=16*ppi, height=10*ppi, res=ppi)
 print(pl_LF_wc)
 dev.off();toc()
 
-### taxon version
+# taxon version ####
+# append taxon info
 tic();set.seed(21);df_tx %>% 
   # group_by(Region) %>% 
   count(.,Taxa) %>% 
@@ -121,43 +128,137 @@ tic();set.seed(21);df_tx %>%
                                   size=14,
                                   colour="red")) -> pl_tx_wc
 
-png(file = "figs/wordcloud_tx_v3.png",
+png(file = "figs/wordcloud_tx_v3.5.png",
     width=16*ppi,
     height=10*ppi,
     res=ppi)
 print(pl_tx_wc)
 dev.off();toc()
 
-### prep taxon data ####
-### remove odd data
-df_tx <- as_tibble(df_tx0) ### create new data (keep df0 as 'raw')
+################################################################
+# WIP ####
+# new version ###
+#append colours
+### append Group variable for colours:
+df_LF0$GROUP <- ifelse(df_LF0$Kingdom=="Chromista","Chromista",
+                      ifelse(grepl("^Fish",df_LF0$LF02),"Fish",
+                             ifelse(grepl("^Cop",df_LF0$LF02),"Copepod",
+                                    ifelse(grepl("^Bryo",df_LF0$LF02),"Bryozoan",
+                                           ifelse(grepl("^Crus",df_LF0$LF02),"Arthropoda",
+                                                  ifelse(grepl("^Clad",df_LF0$LF02),"Arthropoda",
+                                                         ifelse(grepl("^Gast",df_LF0$LF02),"Gastropod",
+                                                                ifelse(grepl("^Poly",df_LF0$LF02),"Annelid",
+                                                                       ifelse(grepl("^Gela",df_LF0$LF02),"Gelatinous",
+                                                                              ifelse(grepl("^Acari",df_LF0$LF02),"Arthropoda","Other")
+                                                                       )))))))))
+df_LF0$cols <- as.factor(ifelse(df_LF0$GROUP=="Annelid", "burlywood4",
+                     ifelse(df_LF0$GROUP=="Arthropoda", "blueviolet",
+                            ifelse(df_LF0$GROUP=="Bryozoan", "sienna",
+                                   ifelse(df_LF0$GROUP=="Chromista", "dodgerblue",
+                                          ifelse(df_LF0$GROUP=="Copepod", "chartreuse",
+                                                 ifelse(df_LF0$GROUP=="Fish", "cyan3",
+                                                        ifelse(df_LF0$GROUP=="Gastropod", "aquamarine2",
+                                                               ifelse(df_LF0$GROUP=="Gelatinous", "deeppink2",
+                                                                      ifelse(df_LF0$GROUP=="Other", "darkgrey","")
+                                                               )))))))))
+pal <- c("burlywood4","blueviolet","sienna","dodgerblue","chartreuse","cyan3",
+                     "aquamarine2","deeppink2","darkgrey")
 
-### convert dates
-df_tx$sample.date <- as.Date(df_tx$sample.date, origin = "1899-12-30")
 
-# Remove 100 µm data [OPTIONAL] ####
-df_tx_100um <- df_tx %>% 
-  filter(str_starts(Sample.comments,"100um"))
+df_LF0 %>%
+  dplyr::select(
+    #Taxa,
+    DisplayName,
+    GROUP,
+    cols) %>%
+  distinct()->tmp_col
 
-df_tx %>% 
-  filter(!str_starts(Sample.comments,"100um")) -> df_tx
+tic();set.seed(21);df_LF0 %>% 
+  # group_by(Region) %>% 
+  # count(.,Taxa) %>%
+  count(.,DisplayName) %>% 
+  mutate(tot=sum(n)) %>% 
+  mutate(prop=n/tot) %>% ungroup() -> tmp
 
-###widen data & fill NAs with 0s ####
-df_tx %>% 
-  dplyr::select(.,-c("Aphia.ID","AbundanceRaw","Taxa","Category":"Unallocated",
-                     "LF0","Kingdom":"Subspecies")) %>% #drop unneeded cols
-  group_by(across(c(-Abund_m3))) %>% # group by everything except abundance
-  summarise(Abund_m3=sum(Abund_m3), #sum abundances
-            .groups="drop") %>% 
-  pivot_wider(names_from = "LF02",values_from = "Abund_m3", #widen
-              values_fill = 0) -> df_tx_w
+tmp <- left_join(tmp,tmp_col, by="DisplayName")
+tmp %>% 
+  ggplot(.,aes(
+    label=DisplayName,
+    size=prop,
+    col=GROUP
+    )
+    )+
+  # geom_text_wordcloud()+
+  geom_text_wordcloud_area()+
+  scale_color_discrete(pal)+
+  # scale_size_area(max_size = 50, trans = power_trans(1/.7)) +
+  # facet_wrap(.~Region, scales = "free")+
+  # scale_size_area(max_size = 20) +
+  scale_size_area(max_size = 70,
+                  trans = power_trans(1/.7)) + #trans = power_trans(1/.7) "better fit human area perception"
+  theme_minimal()+
+  theme(strip.text = element_text(face="bold",
+                                  size=14,
+                                  colour="red")) -> pl_tx_wc
 
-### join & save data ####  
-dfw <- left_join(df_tx_w,df_wims_w,by="PRN")
+png(file = "figs/wordcloud_tx_v4.png",
+    width=16*ppi,
+    height=10*ppi,
+    res=ppi)
+print(pl_tx_wc)
+dev.off();toc()
 
-dftaxa0 <- df_tx %>% 
-  dplyr::select(.,Kingdom:DisplayName,Taxa) %>% 
-  distinct() %>% 
-  mutate_all(as.character) %>%
-  mutate_all(~ replace_na(., "NA"))
-toc()
+#########################
+set.seed(21)
+words <- c(rep("apple", 50), rep("banana", 30), rep("orange", 20))
+groups <- c(rep("Group1", 50), rep("Group2", 30), rep("Group3", 20))
+xdf <- data.frame(word = words, group = groups)
+
+# Plot the word cloud
+ggplot(xdf, aes(label = word, color = group, size = 1)) +
+  geom_text_wordcloud_area() +
+  scale_color_manual(values = c("Group1" = "blue", "Group2" = "green", "Group3" = "red")) +
+  theme_minimal() -> pltest
+png(file = "figs/wordcloud_TEST.png",
+    width=16*ppi,
+    height=10*ppi,
+    res=ppi)
+print(pltest)
+dev.off()
+
+
+
+
+# ### prep taxon data ####
+# ### remove odd data
+# df_tx <- as_tibble(df_tx0) ### create new data (keep df0 as 'raw')
+# 
+# ### convert dates
+# df_tx$sample.date <- as.Date(df_tx$sample.date, origin = "1899-12-30")
+# 
+# # Remove 100 µm data [OPTIONAL] ####
+# df_tx_100um <- df_tx %>% 
+#   filter(str_starts(Sample.comments,"100um"))
+# 
+# df_tx %>% 
+#   filter(!str_starts(Sample.comments,"100um")) -> df_tx
+# 
+# ###widen data & fill NAs with 0s ####
+# df_tx %>% 
+#   dplyr::select(.,-c("Aphia.ID","AbundanceRaw","Taxa","Category":"Unallocated",
+#                      "LF0","Kingdom":"Subspecies")) %>% #drop unneeded cols
+#   group_by(across(c(-Abund_m3))) %>% # group by everything except abundance
+#   summarise(Abund_m3=sum(Abund_m3), #sum abundances
+#             .groups="drop") %>% 
+#   pivot_wider(names_from = "LF02",values_from = "Abund_m3", #widen
+#               values_fill = 0) -> df_tx_w
+# 
+# ### join & save data ####  
+# dfw <- left_join(df_tx_w,df_wims_w,by="PRN")
+# 
+# dftaxa0 <- df_tx %>% 
+#   dplyr::select(.,Kingdom:DisplayName,Taxa) %>% 
+#   distinct() %>% 
+#   mutate_all(as.character) %>%
+#   mutate_all(~ replace_na(., "NA"))
+# toc()
