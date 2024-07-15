0e5db004521a91aea979e4cc2cf5869258baee5c
chriscesar <chris.cesar@environment-agency.gov.uk>
2024-07-15 13:33:15 +0100

    Initial commit. Load WIMS and species data

---
+++ R/imp.load_data_taxa.R
@@ -0,0 +1,68 @@
+# imp.load_data_taxa.R ####
+## load taxon data
+
+tic("Load taxon data, format data and correct taxon names")
+print("Load taxon data and correct taxon names")
+### taxon data
+df_tx0 <- as_tibble(openxlsx::read.xlsx(paste0(datfol,
+                                               "processedData/MBA_Returns_Amalgamated_USE.xlsx"),
+                                        sheet="outR04"))
+
+### append updated taxon names
+tx_chk0 <- as_tibble(openxlsx::read.xlsx(paste0(datfol,
+                                                "processedData/MBA_Returns_Amalgamated_USE.xlsx"),
+                                         sheet="TaxonomicRaw"))
+
+tx_chk <- tx_chk0 %>% 
+  rename(Taxa=ScientificName_accepted,
+         Aphia.ID=AphiaID_accepted) %>% 
+  #dplyr::select(.,-ScientificName) %>% 
+  distinct()
+
+tx_chk %>% dplyr::select(., Taxa, Aphia.ID) %>% 
+  distinct() -> tx_chktrm
+
+df_tx0 <- left_join(df_tx0, tx_chktrm, by="Aphia.ID")
+df_tx0$Taxa.x <- df_tx0$Taxa.y;df_tx0$Taxa.y <- NULL
+df_tx0 %>%
+  rename(Taxa=Taxa.x) %>% 
+  dplyr::select(.,-Abund_m3) %>% 
+  group_by(across(c(!AbundanceRaw))) %>% 
+  summarise(.,AbundanceRaw=sum(AbundanceRaw),.groups = "drop") %>%
+  ungroup() %>% 
+  as_tibble(.) -> df_tx
+rm(tx_chk,tx_chk0,tx_chktrm)
+toc(log=TRUE)
+
+tic("Prep for export & export data");print("Prep for export & export data")
+
+### convert dates
+df_tx$sample.date <- as.Date(df_tx$sample.date, origin = "1899-12-30")
+
+### add month
+df_tx$month <- lubridate::month(df_tx$sample.date)
+df_tx %>% relocate(.,month, .after = sample.date) -> df_tx
+### add season
+df_tx$DJF <- as.factor(seas::mkseas(df_tx$sample.date, width="DJF"))#convert dates to 3month seasonal block
+df_tx %>% relocate(.,DJF, .after = sample.date) -> df_tx
+
+# Remove 100 µm data [OPTIONAL] ####
+df_tx_100um <- df_tx %>% 
+  filter(str_starts(Sample.comments,"100um"))
+
+df_tx %>% 
+  filter(!str_starts(Sample.comments,"100um")) -> df_tx
+
+### set Region as a factor
+df_tx$Region <- factor(df_tx$Region, levels = c("NEast","Anglian","Thames",
+                                                "Southern","SWest","NWest"))
+
+###widen data & fill NAs with 0s ####
+df_tx %>% 
+  dplyr::select(.,-c("Aphia.ID")) %>% 
+  pivot_wider(names_from = "Taxa",values_from = "AbundanceRaw",#Abund_m3
+              values_fill = 0) %>% 
+  filter(.,!is.na(`Net.volume.sampled.(m3)`)) %>% ##OPTIONAL: remove 'empty' net volumes
+  ungroup() -> df_tx_w
+toc(log=TRUE)
+
---
+++ R/imp.load_data_wims.R
@@ -0,0 +1,39 @@
+# imp.load_data_wims.R ####
+## load WIMS data
+
+### WIMS chemical data
+tic("Load WIMS data")
+print("Load WIMS data")
+# WIMS Extract based on:
+# Materials = 2HZZ & 2IZZ; SMPT_TYPE = CD, CC, CE; Dates from 01/06/2022-present
+# SMP_Code:
+# 42100171, 42100174, 42100179, 45400826, 60510027, 73015085, 82510555,
+# 82615055, 82615255, 88002837, 88007163, 88007172, 88025879, BE061099,
+# E0001449, E0001450, E0004730, G0003532, G0003572, LC544405, LC560357,
+# PTTR0026, WA560349, Y0004367, Y0017477, YC536426
+
+df_wims0 <- as_tibble(openxlsx::read.xlsx(paste0(datfol,
+                                                 "/WIMS_Extract_WaterQuality_Zoop_Samples_240618.xlsx"),
+                                          sheet="allDat"))
+
+### counts for pots 1&2 have been summed as to have those for pots 3&4
+### removed whitespace from AphiaIDs & taxon names
+
+### prep data ####
+### format & widen WIMS data ####
+df_wims <- df_wims0
+
+df_wims$PRN <- df_wims$SAMP_SCHEDULE_SAMPLE_ID
+df_wims %>%
+  dplyr::mutate(det=paste0(DETE_DESC,"_",UNIT_SHORT_DESC)) %>% ##create new variable label
+  dplyr::mutate(Result=ifelse(is.na(df_wims$MEAS_SIGN == "<"), MEAS_RESULT,
+                              paste0(MEAS_SIGN,MEAS_RESULT))) %>% #add "<" to results
+  dplyr::select(.,c(WIMS.Code,REGION,Biosys.ID,
+                    SMPT_LONG_NAME, SAMP_SAMPLE_DATE, SAMP_SAMPLE_TIME,
+                    SAMP_Notes,
+                    PRN,det,Result)) %>% ##only keep variables of interest
+  tidyr::pivot_wider(.,names_from=det, values_from = Result) -> df_wims_w###widen data
+
+## write WIMS wide csv
+# write.csv(df_wims_w,file=paste0(datfol,"processedData/WIMS_wide.csv"),row.names = FALSE)
+toc(log=TRUE)
