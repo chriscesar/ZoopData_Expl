f84df2cbb45699f7135669c5e878099c6a077814
chriscesar <chris.cesar@environment-agency.gov.uk>
2024-10-08 15:52:27 +0100

    Ensuring consistency with chart colours

--- R/an.zoopsSizeDistribn.R
+++ R/an.zoopsSizeDistribn.R
@@ -42,7 +42,8 @@ rm(fac_tmp,fac_tmp22,fac_tmp23,fac_tmp24)
 ### make a 'complete' version so that 'missing' surveys will be included in the charts
 dfl_complete <- dfl %>%
   group_by(BIOSYS.Code) %>%  # Group by site
-  tidyr::complete(yyyy_mm, fill = list(mn_carbTot_m3 = 0))  # Fill missing months with 0 or NA
+  tidyr::complete(yyyy_mm, fill = list(mn_carbTot_m3 = 0)) %>% # Fill missing months with 0 or NA
+  tidyr::complete(DJF, fill = list(mn_carbTot_m3 = 0)) #Fill missing seasons
 
 # dftmp_complete %>%
 # # dftmp %>% 
@@ -71,6 +72,7 @@ dfl_complete <- dfl %>%
 # generate pdfs for individual BIOSYS sites ####
 ## MONTLY ####
 # Define the unique levels of BIOSYS.Code (each site)
+tic("Generate & export plots of monthly data by BIOSYS site")
 unique_sites <- unique(dfl_complete$BIOSYS.Code)
 # unique_sites <- unique_sites[20:25]
 
@@ -97,7 +99,8 @@ purrr::walk(unique_sites, function(site) {
          subtitle = paste0("Contribution of zooplankton size classes to total carbon content\n",na.omit(dfl_filtered$Region)[1],": ", na.omit(dfl_filtered$WB)[1],
                            " (",site,")"),
          caption = "Monthly sampling program. Fill colours indicate season.")+
-    scale_fill_manual(values=c("skyblue","lightgreen","orange","sienna"))+
+    scale_fill_manual(values=c("skyblue","lightgreen","orange","sienna"),
+                      limits=c("DJF", "MAM", "JJA", "SON")) +  # Ensure all seasons are present
     scale_y_discrete(limits=rev)+
     theme(axis.title.y = element_blank(),
           legend.position = "none")
@@ -106,8 +109,10 @@ purrr::walk(unique_sites, function(site) {
   ggplot2::ggsave(filename = paste0("figs/2410_zoopSize/",
     "zoopSize_monthly_", na.omit(dfl_filtered$WB_lb)[1],"_",site, ".pdf"), plot = p, width = 8, height = 6)
 })
+toc(log = TRUE)
 
 ## Seasonally ####
+tic("Generate & export plots of seasonal data by BIOSYS site")
 # Define the unique levels of BIOSYS.Code (each site)
 unique_sites <- unique(dfl_complete$BIOSYS.Code)
 #unique_sites <- unique_sites[20:25]
@@ -136,7 +141,8 @@ purrr::walk(unique_sites, function(site) {
          subtitle = paste0("Contribution of zooplankton size classes to total carbon content\n",na.omit(dfl_filtered$Region)[1],": ", na.omit(dfl_filtered$WB)[1],
                            " (",site,")"),
          caption = "Monthly sampling program. Values grouped by season")+
-    scale_fill_manual(values=c("skyblue","lightgreen","orange","sienna"))+
+    scale_fill_manual(values=c("skyblue","lightgreen","orange","sienna"),
+                      limits=c("DJF", "MAM", "JJA", "SON")) +  # Ensure all seasons are present
     scale_y_discrete(limits=rev)+
     theme(axis.title.y = element_blank(),
           legend.position = "none")
@@ -145,3 +151,6 @@ purrr::walk(unique_sites, function(site) {
   ggplot2::ggsave(filename = paste0("figs/2410_zoopSize/",
                                     "zoopSize_seasoanlly_", na.omit(dfl_filtered$WB_lb)[1],"_",site, ".pdf"), plot = p, width = 8, height = 6)
 })
+toc(log = TRUE)
+
+unlist(tictoc::tic.log())
