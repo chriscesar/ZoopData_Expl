fa0f4d676b391feb8066b9f75ff61b5f8c4b7090
chriscesar <chris.cesar@environment-agency.gov.uk>
2024-12-20 15:48:51 +0000

    Update source data location & station codes

--- R/imp.load_data_wims.R
+++ R/imp.load_data_wims.R
@@ -6,23 +6,21 @@ tic("Load WIMS data")
 print("Load WIMS data")
 # WIMS Extract based on:
 # Materials = 2HZZ & 2IZZ; SMPT_TYPE = CD, CC, CE; Dates from 01/06/2022-present
-# SMP_Code:
-# 42100171, 42100174, 42100179, 45400826, 60510027, 73015085, 82510555,
-# 82615055, 82615255, 88002837, 88007163, 88007172, 88025879, BE061099,
-# E0001449, E0001450, E0004730, G0003532, G0003572, LC544405, LC560357,
-# PTTR0026, WA560349, Y0004367, Y0017477, YC536426
 
-# added following WIMS CODEs to list (visited July 2024):
-# Y0003370, Y0004428, G0003451, LC551397
+# Full SMPT codes in data gathered up until October 2024:
+# 42100171,42100174,42100179,45400826,60510027,73015085,
+# 82510555,82615055,82615255,88002837,88007163,88007172,
+# 88025879,BE061099,E0001449,E0001450,E0004730,G0003451,
+# G0003532,G0003572,LC544405,LC551396,LC551397,LC551398,
+# LC560357,PTTR0026,WA560349,Y0003370,Y0004367,Y0004428,
+# Y0017477,YC536426,
 
 df_wims0 <- as_tibble(openxlsx::read.xlsx(paste0(datfol,
                                                  #"/WIMS_Extract_WaterQuality_Zoop_Samples_240618.xlsx"),
                                                  #/"/WIMS_Extract_240618.xlsx"),
-                                                 "/WIMS_Extract_WaterQuality_Zoop_Samples_240930.xlsx"),
+                                                 #"/WIMS_Extract_WaterQuality_Zoop_Samples_240930.xlsx"),
+                                                 "WIMS_Extract_WaterQuality_Zoop_Samples_241217.xlsx"),
                                           sheet="allDat"))
-
-### counts for pots 1&2 have been summed as to have those for pots 3&4
-### removed whitespace from AphiaIDs & taxon names
 
 ### prep data ####
 ### format & widen WIMS data ####
