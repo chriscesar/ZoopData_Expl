794a6eb8645f6aa7f79af9c83b0badc08d08aaf2
chriscesar <chris.cesar@environment-agency.gov.uk>
2025-02-19 17:10:24 +0000

    Tweaks to calculations of orders of magnitude differences

--- R/phytoCarbonComparison.R
+++ R/phytoCarbonComparison.R
@@ -3,7 +3,7 @@
 
 # 00 set up ####
 ## load packages ####
-ld_pkgs <- c("tidyverse", "tictoc")
+ld_pkgs <- c("tidyverse", "tictoc","ggtext","glue")
 
 vapply(ld_pkgs, library, logical(1L),
        character.only = TRUE, logical.return = TRUE);rm(ld_pkgs)
@@ -25,10 +25,13 @@ df0 %>% #View()
                      `ABS_Diff_EA-PML_Vol`)) %>% #names()
   mutate(., Diff = (EA_Mean_C_per_cell_pgC-PML_Mean_C_per_cell_pgC)) %>% #View()
   mutate(., AbsLogDiff = log10(abs(Diff))) %>% #View()
-  mutate(.,LogDiffSign = ifelse(Diff<0,-1*AbsLogDiff,AbsLogDiff)) -> df
+  mutate(.,LogDiffSign = ifelse(Diff<0,-1*AbsLogDiff,AbsLogDiff)) %>% 
+  ## orders of magnitude diffs
+  mutate(.,OrdMagDiff = log10(EA_Mean_C_per_cell_pgC)-log10(PML_Mean_C_per_cell_pgC)) %>% 
+  mutate(.,OrdMagDiffSign = ifelse(Diff<0,-1*OrdMagDiff,OrdMagDiff)) -> df
 
 # plot ####
-png(file = "figs/PhytoCarbonComparison.png",
+png(file = "figs/PhytoCarbonComparison_v1.png",
     width=9*ppi, height=12*ppi, res=ppi)
 df %>% 
   arrange(LogDiffSign) %>% #View() %>% 
@@ -40,13 +43,85 @@ df %>%
   geom_vline(xintercept = seq(-5,5, by = 1),col="grey", lty=3)+
   geom_vline(xintercept = 0, lty=2, col=2)+
   geom_point()+
-  labs(x=expression(bold(Log[10]~"difference in carbon content values")),
-       caption = "Values <0 indicate estimates for which PML values are larger.
-       Values >0 indicate estimates for which EA values are larger.")+
+  scale_x_continuous(breaks = seq(-5, 5, by = 1))+
+  labs(title = "Differences in phytoplankton carbon content estimates for Environment Agency\nand Plymouth Marine Laboratory",
+    x=expression(bold(Log[10]~"difference in carbon content values")),
+       caption = paste0("Values represent the log10 of the absolute differences in estimates between institutions.  Where EA
+       Values <0 indicate estimates for which PML values are larger; values >0 indicate estimates for which EA values are larger.
+                        Values are based on estimates of carbon content (pg per cell) by each institution.
+                        Differences in estimates are provided for the ",nrow(df),
+                        " taxa which have carbon content estimates from both institutions.
+                        The grey area indicates estimates which are within 1 order of magnitude of each other"))+
   theme(
     axis.text.x = element_text(face=2),
     axis.title.x = element_text(face=2),
     axis.title.y= element_blank(),
     axis.text.y = element_blank()
   )
+dev.off()
+
+png(file = "figs/PhytoCarbonComparison_v2.png",
+    width=9*ppi, height=12*ppi, res=ppi)
+# df %>% 
+#   arrange(OrdMagDiff) %>% #View() %>% 
+#   mutate(Name = factor(Name, levels = Name)) %>%  # Reorder factor levels
+#   ggplot(., aes(y = Name, x = OrdMagDiff))+
+#   geom_rect(aes(xmin = -1,xmax = 1, ymin = -Inf,ymax=Inf),fill="lightgrey")+
+#   geom_rect(aes(xmin = -Inf,xmax = -1, ymin = -Inf,ymax=Inf), fill = "lightblue")+
+#   geom_rect(aes(xmin = 1,xmax = Inf, ymin = -Inf,ymax=Inf), fill = "lightgreen")+
+#   geom_vline(xintercept = seq(-3,3, by = 1),col="grey", lty=3)+
+#   geom_vline(xintercept = 0, lty=2, col=2)+
+#   geom_point()+
+#   scale_x_continuous(breaks = seq(-5, 5, by = 1))+
+#   labs(
+#     title = "Differences in phytoplankton carbon content estimates for Environment Agency<br>and Plymouth Marine Laboratory",
+#     x = expression(bold(Log[10]~"difference in carbon content values")),
+#     caption = bquote(
+#       "Orders of magnitude difference calculated using " * Log[10] * "(EA) - " * Log[10] * "(PML)." * "<br>" *
+#         "Values represent the log"[10] * " of the absolute differences in estimates between institutions." * "<br>" *
+#         "Values <0 indicate estimates for which PML values are larger; values >0 indicate estimates for which EA values are larger." * "<br>" *
+#         "Values are based on estimates of carbon content (pg per cell) by each institution." * "<br>" *
+#         "Differences in estimates are provided for the " * .(nrow(df)) * 
+#         " taxa which have carbon content estimates from both institutions." * "<br>" *
+#         "The grey area indicates estimates which are within 1 order of magnitude of each other."
+#     )
+#   ) +
+#   theme(
+#     plot.caption = element_markdown(),
+#     axis.text.x = element_text(face=2),
+#     axis.title.x = element_text(face=2),
+#     axis.title.y= element_blank(),
+#     axis.text.y = element_blank()
+#   )
+
+df %>% 
+  arrange(OrdMagDiff) %>% 
+  mutate(Name = factor(Name, levels = Name)) %>%  # Reorder factor levels
+  ggplot(aes(y = Name, x = OrdMagDiff)) +
+  geom_rect(aes(xmin = -1, xmax = 1, ymin = -Inf, ymax = Inf), fill = "lightgrey") +
+  geom_rect(aes(xmin = -Inf, xmax = -1, ymin = -Inf, ymax = Inf), fill = "lightblue") +
+  geom_rect(aes(xmin = 1, xmax = Inf, ymin = -Inf, ymax = Inf), fill = "lightgreen") +
+  geom_vline(xintercept = seq(-3, 3, by = 1), col = "grey", lty = 3) +
+  geom_vline(xintercept = 0, lty = 2, col = 2) +
+  geom_point() +
+  scale_x_continuous(breaks = seq(-5, 5, by = 1)) +
+  labs(
+    title = "Differences in phytoplankton carbon content estimates for Environment Agency<br>and Plymouth Marine Laboratory",
+    x = expression(bold(Log[10]~"difference in carbon content values")),
+    caption = glue(
+      "Orders of magnitude difference calculated using log<sub>10</sub>(EA) - log<sub>10</sub>(PML).<br>",
+      "Values < 0 indicate estimates for which PML values are larger; values > 0 indicate estimates for which EA values are larger.<br>",
+      "Values are based on estimates of carbon content (pg per cell) by each institution.<br>",
+      "Differences in estimates are provided for the {nrow(df)} taxa which have carbon content estimates from both institutions.<br>",
+      "The grey area indicates estimates which are within 1 order of magnitude of each other."
+    )
+  ) +
+  theme(
+    plot.caption = element_markdown(),  # Enables Markdown rendering for the caption
+    axis.text.x = element_text(face = 2),
+    axis.title.x = element_text(face = 2),
+    axis.title.y = element_blank(),
+    axis.text.y = element_blank()
+  )
+
 dev.off()
